42c2452326d829c8a1789d1071e78eb4e8293db0
Slava ASUS <popovbs@gmail.com>
2025-01-24 22:13:17 +0300

    tasks 1,2,3,4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //Задача 1
         System.out.println("Задача №1:");
         byte b=2;
         short sh=200;
@@ -13,6 +14,7 @@ public class Main {
         System.out.println("Значение переменной l с типом long равно "+l);
         System.out.println("Значение переменной f с типом float равно "+f);
         System.out.println("Значение переменной dd с типом double равно "+dd);
+        //Задача 2
         System.out.println("Задача №2:");
         float bbb=27.12f;
         long lon=987678965549L;
@@ -28,12 +30,25 @@ public class Main {
         System.out.println("Значение переменной fShort с типом short равно "+fShort);
         System.out.println("Значение переменной ddShort с типом short равно "+ddShort);
         System.out.println("Значение переменной byByte с типом byte равно "+byByte);
+        //Задача 3
         System.out.println("Задача №3:");
         short lud=23, ann=27, ekat=30;
         int all=lud+ann+ekat;
         short list=480;
         int each=list/all;
         System.out.println("На каждого ученика рассчитано " +each+ " листов бумаги");
+        //Задача 4
         System.out.println("Задача №4:");
+        int bottle=16;
+        int timeNorma=2;
+        int min20=bottle/timeNorma*20;
+        int day=bottle/timeNorma*60*24;
+        int day3=day*3;
+        int month=day*30;
+        System.out.println("За 20 минут машина произвела " +min20+ " штук бутылок");
+        System.out.println("За сутки машина произвела " +day+ " штук бутылок");
+        System.out.println("За 3 дня машина произвела " +day3+ " штук бутылок");
+        System.out.println("За 1 месяц машина произвела " +month+ " штук бутылок");
+
     }
 }

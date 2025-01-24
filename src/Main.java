e3e78cab80f609b50105080a8f970b3f63cc3220
Slava ASUS <popovbs@gmail.com>
2025-01-24 21:48:28 +0300

    tasks 1,2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Задача №1:");
+        byte b=2;
+        short sh=200;
+        int in=sh*b;
+        long l=5000L;
+        float f=2.5f;
+        double dd=f*f;
+        System.out.println("Значение переменной b с типом byte равно "+b);
+        System.out.println("Значение переменной sh с типом short равно "+sh);
+        System.out.println("Значение переменной in с типом int равно "+in);
+        System.out.println("Значение переменной l с типом long равно "+l);
+        System.out.println("Значение переменной f с типом float равно "+f);
+        System.out.println("Значение переменной dd с типом double равно "+dd);
+        System.out.println("Задача №2:");
+        float bbb=27.12f;
+        long lon=987678965549L;
+        double inFloat=2.786;
+        short lShort=569;
+        short fShort=-159;
+        short ddShort=27897;
+        byte byByte=57;
+        System.out.println("Значение переменной bbb с типом float равно "+bbb);
+        System.out.println("Значение переменной lon с типом long равно "+lon);
+        System.out.println("Значение переменной inFloat с типом double равно "+inFloat);
+        System.out.println("Значение переменной lShort с типом short равно "+lShort);
+        System.out.println("Значение переменной fShort с типом short равно "+fShort);
+        System.out.println("Значение переменной ddShort с типом short равно "+ddShort);
+        System.out.println("Значение переменной byByte с типом byte равно "+byByte);
+        System.out.println("Задача №3:");
     }
 }
